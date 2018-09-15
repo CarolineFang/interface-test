6b2128e5916cffc3d36215ebc689118c3cc1551c
yann <jasminesyann@gmail.com>
2018-09-15 14:15:53 +0800

    Unittest

---
+++ Unittest/test_exception.py
@@ -0,0 +1,10 @@
+import unittest
+
+class DivZeroTestCase(unittest.TestCase):
+
+    def test_should_raise_exception(self):
+        with self.assertRaises(ZeroDivisionError):
+            1 / 0
+
+if __name__ == '__main__':
+    unittest.main()
